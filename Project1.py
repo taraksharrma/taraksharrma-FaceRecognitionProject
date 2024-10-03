85409c1d2a26c76791fb912fdd2b74ba95f2fda6
Tarak Sharma <taraksharma.work@gmail.com>
2024-10-03 10:34:11 +0530

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,71 @@
+import face_recognition
+import cv2
+import numpy as np
+import csv
+from datetime import datetime
+
+video_capture = cv2.VideoCapture(0)
+
+# Load Known Faces
+tarak_image = face_recognition.load_image_file("faces/tarak.PNG")
+tarak_encoding = face_recognition.face_encodings(tarak_image)[0]
+
+known_face_encodings = [tarak_encoding]
+known_face_names = ["tarak"]
+
+# List of expected students
+students = known_face_names.copy()
+
+face_locations = []
+face_encodings = []
+
+# Get the current date and time
+now = datetime.now()
+current_date = now.strftime("%d.%m.%Y")
+
+f = open(f"{current_date}.csv", "w+", newline="")
+lnwriter = csv.writer(f)
+
+while True:
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+    # Recognize face
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        face_distance = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distance)
+
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+            # Add the text if the person is present
+            if name in known_face_names:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                bottomLeftCornerOfText = (10, 100)
+                fontScale = 1.5
+                fontColor = (255, 0, 0)
+                thickness = 3
+                lineType = 2
+                cv2.putText(frame, name + " Present", bottomLeftCornerOfText, font, fontScale, fontColor, thickness, lineType)
+
+                if name in students:
+                    students.remove(name)
+                    current_time = now.strftime("%H-%M-%S")
+                    lnwriter.writerow([name, current_time])
+
+    # Show the resulting frame
+    cv2.imshow("Attendance", frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# Release the video capture object and close all OpenCV windows
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
